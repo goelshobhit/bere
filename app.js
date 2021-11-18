99e6148e893fc7be9b2f9b90b2792a7a29ffa038
Paul <vuquochung1918@outlook.com>
2021-11-18 11:22:03 +0700

    resolved the conflict

--- app.js
+++ app.js
@@ -222,6 +222,9 @@ require("./app/routes/post_reaction.routes")(app);
 require("./app/routes/post_report.routes")(app);
 require("./app/routes/audit_log.routes")(app);
 require("./app/routes/search.routes")(app);
+require("./app/routes/notify_grp.routes")(app);
+require("./app/routes/notify_event.routes")(app);
+require("./app/routes/notify_trig.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3030;
 const server = https.createServer({
