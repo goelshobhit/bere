262ca383aa50045c57de2977d21653725dad2307
Paul <paul@socialapp.com>
2021-12-07 17:33:50 +0700

    Models of the following tables: bonus_rewards, bonus_task, bonus_ticket_rules, bonus_ticket. Route and controller of the bonus ticket.

---
+++ app/models/bonus_ticket_rules.model.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, Sequelize) => {
+  const bonus_tickets_rules = sequelize.define("bonus_tickets_rules", {
+    bonus_tickets_rules_id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    bonus_tickets_rules_id: {
+      allowNull: false,
+      type: Sequelize.INTEGER
+    },
+    bonus_tickets_rules: {
+      type: Sequelize.STRING(255)
+    },
+    bonus_tickets_how_it_works: {
+      type: Sequelize.STRING(255)
+    },
+    bonus_tickets_cashout_rules: {
+      type: Sequelize.STRING(255)
+    }
+  }, {
+      createdAt: 'bonus_tickets_rules_created_at',
+      updatedAt: 'bonus_tickets_rules_updated_at',
+      freezeTableName: true,
+      tableName: 'bonus_tickets_rules',
+      underscored: true
+  });
+  return bonus_tickets_rules;
+}
