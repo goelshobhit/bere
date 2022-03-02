6eacee3a53523cd3d28fc98392c5b4ca40d36b07
Canada123 <Socialbrands1@gmail.com>
2022-03-02 23:24:56 +0530

    Update admin.routes.js

--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -26,7 +26,7 @@ module.exports = app => {
      *                        User role:
      *                            type: integer
      *     tags:
-     *       - Admin Users
+     *       - Admin userss
      *     description: Add new user
      *     produces:
      *       - application/json
