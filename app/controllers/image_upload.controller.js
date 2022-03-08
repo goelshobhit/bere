fd33a28749322e434eea7ff33e82f995c13807b0
Manpreet <manpreet.intersoft@gmail.com>
2022-03-08 16:23:33 +0530

    image upload functionality changes

--- app/controllers/image_upload.controller.js
+++ app/controllers/image_upload.controller.js
@@ -1,40 +1,71 @@
 
 
 const sharp = require('sharp');
-const uploadImage = require('../helpers/image_helpers')
+const uploadImage = require('../helpers/image_helpers');
+const { data } = require('../middleware/logger');
+const gc = require('../gc-config/')
+const bucket = gc.bucket('dev-server-image-bucket')
 
 
 exports.imagesUpload = async (req, res) => {
     try {
-        const myFile = req.file
+        const myFiles = req.files
         if (!req.body.media_key) {
             return res.status(400).send({
                 message: "media_key is required"
             });
         }
-        if (!req.file) {
+        if (!req.files) {
             return res.status(400).send({
                 message: "At least one file required"
             });
         }
-        myFile.originalname = `${Date.now()}-${myFile.originalname}`;
-        const imageUrl = await uploadImage(myFile)
-        const thumbnail = {
-            fieldname: myFile.fieldname,
-            originalname: `thumbnail/${myFile.originalname}`,
-            encoding: myFile.encoding,
-            mimetype: myFile.mimetype,
-            buffer: await sharp(myFile.buffer).resize(465, 360).toBuffer()
-          }
-        const imagethumbnailUrl = await uploadImage(thumbnail);
+        let imageAllData = [];
+        for (var i in myFiles) {
+            var imageData = {};
+            var myFile = myFiles[i];
+            myFile.originalname = `${Date.now()}-${myFile.originalname}`;
+            const imageUrl = await uploadImage(myFile)
+            const thumbnail = {
+                fieldname: myFile.fieldname,
+                originalname: `thumbnail/${myFile.originalname}`,
+                encoding: myFile.encoding,
+                mimetype: myFile.mimetype,
+                buffer: await sharp(myFile.buffer).resize(465, 360).toBuffer()
+            }
+            const imagethumbnailUrl = await uploadImage(thumbnail);
+            var url_parts = imageUrl.split('/');
+            var imageName = url_parts.pop();  
+            imageData['image_name'] = imageName;
+            imageData['image_url'] = imageUrl;
+            imageData['thumbnail_url'] = imagethumbnailUrl;
+            imageAllData.push(imageData);
+        }
+        
         res.status(200).json({
             message: "Upload was successful",
-            data: imageUrl,
-            thumbnail: imagethumbnailUrl
+            data: imageAllData
+            //thumbnail: imagethumbnailUrl
           })
       } catch (error) {
         return res.status(500).send({
                         message: `Error when trying  file: ${error}`
                     });
       }
+}
+
+exports.deleteImage = async (req, res) => {
+    try {
+        var fileName = req.params.imageName;
+        var thumbnailfileName = `thumbnail/${fileName}`
+        await bucket.file(fileName).delete();
+        await bucket.file(thumbnailfileName).delete();
+        res.status(200).json({
+            message: "image deleted"
+          });
+    } catch (error) {
+        return res.status(500).send({
+                        message: `Error when deleting  file: ${error}`
+                    });
+        }
 }
--- app/routes/image_upload.routes.js
+++ app/routes/image_upload.routes.js
@@ -2,7 +2,7 @@ module.exports = app => {
   const image_upload = require("../controllers/image_upload.controller.js");
   var router = require("express").Router();
   require("dotenv").config();
-
+  const auth = require("../middleware/auth");
 const bodyParser = require('body-parser')
 const multer = require('multer')
 
@@ -16,14 +16,15 @@ const multerMid = multer({
 });
 
 app.disable('x-powered-by')
-app.use(multerMid.single('file'))
+//app.use(multerMid.single('file'))
+app.use(multerMid.array('file', 12))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 
  	/**
 	* @swagger
-	* /api/image-upload:
+	* /api/images-upload:
 	*   post:
 	*     requestBody:
 	*        required: true
@@ -61,6 +62,38 @@ app.use(bodyParser.urlencoded({extended: false}))
 	*                              type: string
 	*                              example: Authorisation Required
 	*/
-  router.post("/image-upload", image_upload.imagesUpload);
+  router.post("/images-upload", auth, image_upload.imagesUpload);
+
+  /**
+   * @swagger
+   * /api/images-upload/{imageName}:
+   *   delete:
+   *     parameters:
+   *         - name: imageName
+   *           in: path
+   *           required: true
+   *           schema:
+   *              type: integer
+   *     tags:
+   *       - File Upload
+   *     description: Delete Image From GCP
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: Delete Image From GCP
+   *       401:
+   *          description: Unauthorized
+   *          content:
+   *              application/json:
+   *                  schema:
+   *                      type: object
+   *                      properties:
+   *                          message:
+   *                              type: string
+   *                              example: Authorisation Required
+   */
+  router.delete("/images-upload/:imageName", auth, image_upload.deleteImage); 
+
   app.use("/api", router);
 };
