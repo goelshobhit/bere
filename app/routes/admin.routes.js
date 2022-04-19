e3f52aeb66d65e034a09f438e20d1327d002c87f
Canada123 <Socialbrands1@gmail.com>
2022-04-19 23:17:21 +0530

    Update admin.routes.js
    testing ci/cid

--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -26,7 +26,7 @@ module.exports = app => {
      *                        User role:
      *                            type: integer
      *     tags:
-     *       - Admin Userss
+     *       - Admin Users
      *     description: Add new user
      *     produces:
      *       - application/json
