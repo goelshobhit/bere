b2cf9c9289cb06632545a000d9c57a12e11a8fee
Manpreet <manpreet.intersoft@gmail.com>
2022-05-09 10:39:40 +0530

    swagger link change

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ const swaggerOptions = {
 };
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 // app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/swagger-documentation", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Social App api application." });
